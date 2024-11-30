d8a598735748701c0095c6dc52ef2a99870a0b79
Luke Heuser <103976088+LukeHeuser@users.noreply.github.com>
2024-11-30 18:01:28 -0500

    Sixth Commit

--- src/heuser/luke/MoreLists.java
+++ src/heuser/luke/MoreLists.java
@@ -1,11 +1,14 @@
 package heuser.luke;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 
 public class MoreLists {
 
     public static void main(String[] args) {
+
 
 
         String[] items = {"apples", "bananas", "milk", "eggs"};
@@ -60,16 +63,29 @@ public class MoreLists {
         groceries.remove("yogurt");
         System.out.println(groceries);
 
+
         groceries.removeAll(List.of("apples", "eggs"));
         System.out.println(groceries);
 
         groceries.retainAll(List.of("apples", "milk", "mustard", "cheese"));
-
         System.out.println(groceries);
 
         groceries.clear();
         System.out.println(groceries);
         System.out.println("isEmpty = " + groceries.isEmpty());
+
+        groceries.addAll(List.of("apples", "milk", "mustard", "cheese"));
+        groceries.addAll(Arrays.asList("eggs", "pickles", "mustard", "ham"));
+
+        System.out.println(groceries);
+        groceries.sort(Comparator.naturalOrder());
+        System.out.println(groceries);
+
+        groceries.sort(Comparator.reverseOrder());
+        System.out.println(groceries);
+
+        var groceryArray = groceries.toArray(new String[groceries.size()]);
+        System.out.println(Arrays.toString(groceryArray));
 
     }
 }
